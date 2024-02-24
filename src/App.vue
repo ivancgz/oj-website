3c97eec038c5f2e8b3998234b0f9c0c4f4917e60
ivan <ivancgz@163.com>
2024-02-25 04:05:39 +0800

    优化权限管理页面和用户权限

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,16 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import { useRouter } from "vue-router";
-import { useStore } from "vuex";
+import {onMounted} from "vue";
 
-const router = useRouter();
-const store = useStore();
+/**
+ * 全局初始化函数, 有全局单次调用的代码, 都可以写在这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到 Ivan 的项目");
+};
 
-router.beforeEach((to, from, next) => {
-  // 仅管理员可见, 判断当前用户是否有权限
-  console.log(to);
-  if (to.meta?.access === "canAdmin") {
-    if (store.state.user.loginUser?.role !== "admin") {
-      console.log(store.state.user.loginUser);
-      next("/noAuth");
-      return;
-    }
-  }
-  next();
+onMounted(() => {
+  doInit();
 });
 </script>
