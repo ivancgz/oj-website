fbfb658f2de4143b4b5602b55d71d89e7af5a82e
ivan <ivancgz@163.com>
2024-02-25 19:53:41 +0800

    æ·å®用户登录界面, 前后端é联调

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import "@arco-design/web-vue/dist/arco.css";
 import router from "./router";
 import store from "./store";
 import "@/plugins/axios";
+import "@/access";
 
 createApp(App).use(store).use(router).use(ArcoVue).mount("#app");
---
+++ src/views/user/UserRegisterView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">用户注册页</div>
+</template>
+
+<script lang="ts"></script>
