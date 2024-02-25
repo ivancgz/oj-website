180a606d03a13bcce317206ab10ab1a72692a58a
ivan <ivancgz@163.com>
2024-02-25 17:08:19 +0800

    引入OpenAPI 自动生成请求代码

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 </style>
 <script setup lang="ts">
 import BasicLayout from "@/layouts/BasicLayout.vue";
-import {onMounted} from "vue";
+import { onMounted } from "vue";
 
 /**
  * 全局初始化函数, 有全局单次调用的代码, 都可以写在这里
